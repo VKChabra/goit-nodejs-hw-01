6538708243b8ef2d40820880b2fc5e3f5e6f01d0
Vlad Koblay <vkchabraww@gmail.com>
2022-09-10 14:13:14 +0300

    done

--- contacts.js
+++ contacts.js
@@ -1,21 +1,58 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-const contactsPath = path("db/contacts.json");
+const contactsPath = path.resolve("./db/contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+const getAllContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+const rewriteContacts = (data) =>
+  fs.writeFile(contactsPath, data, (err) => {
+    if (err) console.log(err);
+  });
+
+async function listContacts() {
+  const contacts = await getAllContacts();
+  console.table(contacts);
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  const contacts = await getAllContacts();
+  const contactById = contacts.find((contact) => contact.id === `${contactId}`);
+  if (!contactById) throw new Error(`Contact with ${contactId} not found`);
+  console.log(contactById);
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  const contacts = await getAllContacts();
+  const contactToRemoveIndex = contacts.findIndex(
+    (contact) => contact.id === `${contactId}`
+  );
+  if (contactToRemoveIndex === -1)
+    throw new Error(`Contact with ${contactId} not found`);
+
+  const filteredContacts = contacts.filter(
+    (_, index) => index !== contactToRemoveIndex
+  );
+  rewriteContacts(JSON.stringify(filteredContacts));
+  console.log(contacts[contactToRemoveIndex]);
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name = null, email = null, phone = null) {
+  const contacts = await getAllContacts();
+  const newContact = { id: uuidv4(), name, email, phone };
+  const updatedContacts = JSON.stringify([...contacts, newContact]);
+  rewriteContacts(updatedContacts);
+  console.log(newContact);
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
